61318761f1f8010222a5cbfc861e188653ac5305
forati89 <angel3r@hotmail.com>
2019-09-17 13:49:16 +0200

    add new visuals

--- src/components/WeatherFiveDay.js
+++ src/components/WeatherFiveDay.js
@@ -13,33 +13,33 @@ export default class WeatherFiveDay extends Component {
             <b>Datum: {this.props.d1D}</b>
             <Image src={this.props.d1CI} rounded />
             <b></b>
-            <p>Max Tempratur: <b>{this.props.d1MXT}</b></p>
-            <p>Min Tempratur: <b>{this.props.d1MNT}</b></p>
+            <p>Max Tempratur: <b>{this.props.d1MXT} &#8451;</b></p>
+            <p>Min Tempratur: <b>{this.props.d1MNT} &#8451;</b></p>
             </Col>
             <Col >
             <b>Datum: {this.props.d2D}</b>
             <Image src={this.props.d2CI} rounded />
             <b></b>
-            <p>Max Tempratur: <b>{this.props.d2MXT}</b></p>
-            <p>Min Tempratur: <b>{this.props.d2MNT}</b></p>
+            <p>Max Tempratur: <b>{this.props.d2MXT} &#8451;</b></p>
+            <p>Min Tempratur: <b>{this.props.d2MNT} &#8451;</b></p>
             </Col>
             <Col >
             <b>Datum: {this.props.d3D}</b>
             <Image src={this.props.d3CI} rounded />
-            <p>Max Tempratur: <b>{this.props.d3MXT}</b></p>
-            <p>Min Tempratur: <b>{this.props.d3MNT}</b></p>
+            <p>Max Tempratur: <b>{this.props.d3MXT} &#8451;</b></p>
+            <p>Min Tempratur: <b>{this.props.d3MNT} &#8451;</b></p>
             </Col>
             <Col >
             <b>Datum: {this.props.d4D}</b>
             <Image src={this.props.d4CI} rounded />
-            <p>Max Tempratur: <b>{this.props.d4MXT}</b></p>
-            <p>Min Tempratur: <b>{this.props.d4MNT}</b></p>
+            <p>Max Tempratur: <b>{this.props.d4MXT} &#8451;</b></p>
+            <p>Min Tempratur: <b>{this.props.d4MNT} &#8451;</b></p>
             </Col>
             <Col >
             <b>Datum: {this.props.d5D}</b>
             <Image src={this.props.d5CI} rounded />
-            <p>Max Tempratur: <b>{this.props.d5MXT}</b></p>
-            <p>Min Tempratur: <b>{this.props.d5MNT}</b></p>
+            <p>Max Tempratur: <b>{this.props.d5MXT} &#8451;</b></p>
+            <p>Min Tempratur: <b>{this.props.d5MNT} &#8451;</b></p>
             </Col>
             </Row>
             </React.Fragment>
