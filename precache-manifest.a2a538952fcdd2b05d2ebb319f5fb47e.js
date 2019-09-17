8f53f89a4c8ca7717b78cf67c3015215b0803c94
forati89 <angel3r@hotmail.com>
2019-09-17 13:44:33 +0200

    Updates

---
+++ precache-manifest.a2a538952fcdd2b05d2ebb319f5fb47e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4d7469c469fbe1f4aca8beb2ca8154",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "68f3c0d415dcc3fb45be",
+    "url": "/WeatherForecast/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "68f3c0d415dcc3fb45be",
+    "url": "/WeatherForecast/static/js/2.3c1da57a.chunk.js"
+  },
+  {
+    "revision": "0dcbdbbc4252be75e1dc",
+    "url": "/WeatherForecast/static/js/main.fd448154.chunk.js"
+  },
+  {
+    "revision": "0a0733ae6dfb5212b46e",
+    "url": "/WeatherForecast/static/js/runtime~main.3eae427b.js"
+  }
+]);
